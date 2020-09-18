4cd03f73a9e7f9e07dcc7a1c96567fc58aa09be0
Jefferson Lopes de Sousa <jeffersonpp@gmail.com>
2020-09-18 12:10:20 -0700

    First Commit
    Simple Car / Make
    with make object
    car object

---
+++ src/Entity/Car.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CarRepository::class)
+ */
+class Car
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Make::class, inversedBy="cars")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $make;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMake(): ?Make
+    {
+        return $this->make;
+    }
+
+    public function setMake(?Make $make): self
+    {
+        $this->make = $make;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Make.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MakeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MakeRepository::class)
+ */
+class Make
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $Name;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Car::class, mappedBy="make", orphanRemoval=true)
+     */
+    private $cars;
+
+    public function __construct()
+    {
+        $this->cars = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Car[]
+     */
+    public function getCars(): Collection
+    {
+        return $this->cars;
+    }
+
+    public function addCar(Car $car): self
+    {
+        if (!$this->cars->contains($car)) {
+            $this->cars[] = $car;
+            $car->setMake($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCar(Car $car): self
+    {
+        if ($this->cars->contains($car)) {
+            $this->cars->removeElement($car);
+            // set the owning side to null (unless already changed)
+            if ($car->getMake() === $this) {
+                $car->setMake(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function __toString() {
+        return $this->Name;
+    }
+}
